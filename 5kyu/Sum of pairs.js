c719373001b1338c083ba7c56ddf70305667b8e3
BARIS <barissavas17@gmail.com>
2021-06-25 01:10:11 +0300

    kata solved

--- 5kyu/Sum of pairs.js
+++ 5kyu/Sum of pairs.js
@@ -1,24 +1,11 @@
 function sumPairs(ints, s) {
-    let result = [];
+    let lastNumber
     for (let i = 0; i < ints.length; i++) {
-        const element = ints[i];
-        for (let j = i+1; j < ints.length ; j++) {
-            const innerElement = ints[j];
-            if ( element + innerElement == s ) {
-                result.push([element,innerElement, [i,j]])
-                
-            }
-        }
-        
+      if (lastNumber && lastNumber === ints[i]) continue
+      lastNumber = ints[i]
+      const sliced = ints.slice(0, i)
+      const sumIndex = sliced.indexOf(s - ints[i])
+      if (sumIndex !== -1) return [sliced[sumIndex], ints[i]]
     }
-    let indices = result.map((e) => {Math.abs(e[2][0] - e[2][1]), e.slice(0,2)})
-    indices 
-  return indices;
- /*    return  indices[1][0] < indices[0][0] ? indices[1][1] :
-    indices[1][0] > indices[0][0] ? indices[0][1] : undefined
-    return ( result.length > 1) ? result : undefined */
-    
-}
-    
-
-  console.log(sumPairs([1, 4, 8, 7, 3, 15], 8))
+  }
+  console.log(sumPairs([1, -2, 3, 0, -6, 1], -6))
