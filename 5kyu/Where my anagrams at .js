6421313a79be9aa53d2464e650a0dac25bdb8136
BARIS <barissavas17@gmail.com>
2021-07-08 00:19:42 +0300

    kata solved

---
+++ 5kyu/Where my anagrams at .js
@@ -0,0 +1,40 @@
+/* function anagrams(word, words) {
+const checker = words.filter(e=>e.split('').sort().join('') ===
+ word.split('').sort().join(''));
+return checker
+} */
+
+///////////////////////////////////
+
+/* String.prototype.sort = function() {
+  return this.split("").sort().join("");
+};
+
+function anagrams(word, words) {
+  return words.filter(function(x) {
+      return x.sort() === word.sort();
+  });
+}
+
+console.log( anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])) */
+///////////////////////////////////
+
+/* function anagrams(word, words) {
+  var result = [];
+  var test = word.split("").sort().join("");
+  
+ let map = words.filter((e) => e.split('').sort().join('') == test)
+  return map;
+}
+console.log( anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])) */
+
+///////////////////////////////////
+
+/* function anagrams(word, words) {
+  return words.filter(w => reorder(w) === reorder(word));
+}
+function reorder(word) {
+  return word.split('').sort().join('');
+} */
+
+///////////////////////////////////
