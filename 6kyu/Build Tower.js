ec205f909c7700775b107d7f0f9414ba17118dc0
BARIS <barissavas17@gmail.com>
2021-07-11 17:27:42 +0300

    kata solved

---
+++ 6kyu/Build Tower.js
@@ -0,0 +1,11 @@
+function towerBuilder(floors){
+  let space,star, tower = [];
+  for(i = 1; i <= floors; i++){
+    space = " ".repeat(floors - i);
+    star  = "*".repeat((2*i) - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+
+  console.log(towerBuilder(3))
