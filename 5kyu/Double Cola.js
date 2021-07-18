3409f1d609b330d68d1e83e210911ab04cfb99a7
BARIS <barissavas17@gmail.com>
2021-07-18 15:43:36 +0300

    kata solved

---
+++ 5kyu/Double Cola.js
@@ -0,0 +1,15 @@
+/* function whoIsNext(names, r){
+    let peopleInLine = names.length;
+   let copiesOfEachPerson = 1;
+   
+   while (r > peopleInLine) {
+     r -= peopleInLine;
+     copiesOfEachPerson *= 2;
+     peopleInLine *= 2;
+   }
+   
+   return names[Math.floor((r - 1) / copiesOfEachPerson) % peopleInLine];
+ }
+ */
+
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52))
