5b9a1a93f6a3a987a8e77a8e1c6383f1b1f1b39a
BARIS <barissavas17@gmail.com>
2021-08-04 00:22:06 +0300

    new solution added

--- 5kyu/convert hex to RGB.js
+++ 5kyu/convert hex to RGB.js
@@ -1,5 +1,5 @@
 
-function hexStringToRGB(h) {
+/* function hexStringToRGB(h) {
     return {
       r: parseInt(h.slice(1,3), 16),
       g: parseInt(h.slice(3,5), 16),
@@ -7,4 +7,9 @@ function hexStringToRGB(h) {
     };  
   }
 
+  console.log(hexStringToRGB('#FF9933')) */
+
+  const hexStringToRGB = hexString =>
+  (([r, g, b]) => ({r, g, b}))
+  (hexString.match(/\w{2}/g).map(val => parseInt(val, 16)));
   console.log(hexStringToRGB('#FF9933'))
