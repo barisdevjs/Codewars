96eaf69a5e4a05eca0ae0d51db0c07a33e4b4ec8
BARIS <barissavas17@gmail.com>
2021-09-08 16:14:04 +0300

    kata solved

---
+++ 6kyu/multiplicationtable.js
@@ -0,0 +1,28 @@
+/* const multiplicationTable = function(size) {
+if ( size === 1 ) return  [ [ 1 ] ]
+let result = []
+let arr = []
+if (size > 1) { 
+    for (let index = 0; index < size; index++) {
+        const element = index + 1
+        arr.push(element)
+    }
+  }
+  
+for (let i = 0; i < size; i++) {
+    const element = i + 1
+    result.push(arr.map((e) => e * element))
+}
+return result
+} */
+
+/* multiplicationTable = function(size) {
+
+    return Array.apply(null, new Array(size)).map(function(val, i) {
+      return Array.apply(null, new Array(size)).map(function(val, j) {
+        return (i+1) * (j+1);
+      });
+    });
+  } */
+
+// console.log(multiplicationTable(4))
