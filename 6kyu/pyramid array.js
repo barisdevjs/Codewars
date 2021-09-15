91feadab08e8d8d46657d6385798e1d5b38016a0
BARIS <barissavas17@gmail.com>
2021-09-15 22:06:47 +0300

    kata solved

---
+++ 6kyu/pyramid array.js
@@ -0,0 +1,14 @@
+/* function pyramid(n) {
+    let res = [];
+    for(let i = 0; i < n; i++)
+      res.push([...Array(i+1)].fill(1));
+      res
+    return res;
+  }
+console.log(pyramid(3)) */
+
+/* function pyramid(n) {
+    return Array(n).fill().map((e,i)=>Array(i+1).fill('s'))
+}
+console.log(pyramid(3)) */
+
