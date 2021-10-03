999c8bc44ebc960934b2fcbea238b28f12e9ae63
BARIS <barissavas17@gmail.com>
2021-10-03 20:56:28 +0300

    kata solved

--- 5kyu/decimal to Factorial and Back.js
+++ 5kyu/decimal to Factorial and Back.js
@@ -1,22 +1,34 @@
-function dec2FactString(nb) {
-function recursive(number,divider,resultInside) {
-let number = nb
-let divider = 1
-let remainder = number % divider
-let resultArr = []
-    resultInside = Math.floor(number / divider)
-    if ( resultInside < 1) break
-    recursive(number ,divider) {
-        resultArr.push(resultInside)
+function dec2FactString(nb) {   // will be solved
+    const legend = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let str = '0';
+    let i = 2;
+    while(nb){
+       str = legend[nb%i] + str;
+       nb = Math.floor(nb / i);
+       i++;
+    };
+    return str; 
     }
-    return resultArr
-}
-return resultArr
-}
-console.log(dec2FactString(463))
+console.log(dec2FactString( 1504746))
 
+function factString2Dec(str) {
+const arr = str.split('').reverse()
+let result = 0
+arr
+function factorial(n){
+    //base case
+    if(n == 0 || n == 1){
+        return 1;
+    //recursive case
+    }else{
+        return n * factorial(n-1);
+    }
+}
 
-/* function factString2Dec(str) {
-	// your code
-	return 1;
-} */
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    result += Number(arr[i]) * factorial(i)
+}
+ return result 
+}
+console.log(factString2Dec('3999999999'))
