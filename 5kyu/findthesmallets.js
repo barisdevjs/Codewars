8922081c2753a7092b40f44e60069850f66b910b
BARIS <barissavas17@gmail.com>
2021-10-04 03:16:35 +0300

    solution

--- 5kyu/findthesmallets.js
+++ 5kyu/findthesmallets.js
@@ -1,39 +1,18 @@
 function smallest(n) {
-let arr = String(n).split('').map((e) => Number(e));
-let max =  Math.max(...arr)
-let min =  Math.min(...arr)
-const idxMax = arr.indexOf(max)
-const idxMin = arr.indexOf(min)
-if ( min == 0) { arr.splice(idxMin,1)
-return [Number(arr.join('')), 0, idxMin] 
-}
-// CANT SOLVE
-let result = []
-if ( arr[0] === min ) {
-    const realMin = arr.splice(0,1)
-    arr
-    for (let i = 0; i < arr.length; i++) {
-        const leftMin = Math.min(...arr)
-        const idxMin = arr.indexOf(leftMin)
-        var willReplaced = arr.splice(idxMin,1)
-        willReplaced
-        arr.unshift(willReplaced)
-        arr = arr.flat(Infinity)
-        break   
-    }
-     arr = realMin.concat(arr)
-    return [Number(arr.join('')), Number(willReplaced) +1  , 1]
-}
-for (let i = 0; i < idxMin; i++) {
-    let element = arr[i];
-    if ( element > min) {
-        arr.splice(idxMin,1)
-        arr.unshift(min)
-        result.push(Number(arr.join('')), idxMin, i)
-        break
-    }
-}
-return result
-}
-console.log(smallest(1000000))
-// [239687, 4, 1]
+    const s = [...''+n], smallest = [n,0,0]
+    for (let i = 0; i < s.length; i++)
+      for (let j = 0; j < s.length; j++) {
+        if (i === j) j++
+        const t = s.slice()
+        t.splice(j,0,t.splice(i,1)[0])
+        const m = +t.join``
+        if (m < smallest[0]) 
+          smallest.splice(0,3,m,i,j)
+      }
+    return smallest
+  }
+
+console.log(smallest(285365))
+// [238565, 3, 1]
+//console.log(smallest(269045))
+// [26945, 3, 0]
