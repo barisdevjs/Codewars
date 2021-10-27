c1a695b69ab103e809fe0f1c01e99e1353a06079
Barış SAVAŞ <barissavas17@gmail.com>
2021-10-27 20:27:35 +0300

    kata solved

--- 4kyu/Smallest possible sum.js
+++ 4kyu/Smallest possible sum.js
@@ -1,22 +1,26 @@
-function solution(numbers) {
-    if (numbers.includes(1)) { return numbers.length}
+function gcd (a, b) {
+  if(b == 0){
+      return a;
+  }
+  return gcd(b, a%b);
+}
+function gcd_more_than_two_numbers (a) {
+return a.reduce(gcd)
+}
 
+function solution(numbers) {
+  let solution =  numbers.length
+  const gccc = gcd_more_than_two_numbers(numbers);
+  if ( gccc > 1) { solution = numbers.length * gccc }
+    if (numbers.includes(1)) { return numbers.length}
     if(equalNums(numbers)){
         return numbers.reduce(function(a,b){
           return a + b;
           });
-        } else {
-          const sorted =numbers.map((a,b) => a-b)
-          
-
-          
-          
-        }
-
+        } 
+        return solution
 }
 	
-
-
 function equalNums(numbers){
 return numbers.reduce(function(a, b){ return (a === b) ? a : NaN; });
 }
