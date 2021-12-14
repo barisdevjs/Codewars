b2c61c87926f156d005ac918521b4eca9dd9244b
Barış SAVAŞ <barissavas17@gmail.com>
2021-12-14 19:57:47 +0300

    draft challenge

---
+++ Draft/anagrams.js
@@ -0,0 +1,17 @@
+function isAnagram(stringOne, stringTwo) {
+    const arrOne = stringOne.split('').sort((a,b) => a -b)
+    const arrTwo = stringTwo.split('').sort((a,b) => a -b)
+    
+    return arrOne.every((e, idx) => arrTwo.includes(e)) &&
+      arrTwo.every((e, idx) => arrOne.includes(e))
+      
+      ? true : false
+  }
+  
+  console.groupCollapsed(
+    (isAnagram("rasp", "spar"), true),
+    (isAnagram("", ""), true),
+    (isAnagram("gslkdj", "asdlfkjgj"), false),
+    (isAnagram("tom marvolo riddle", "i am lordvoldemort"),true),
+    (isAnagram("hello", "hdkwl"), false ),
+  )
