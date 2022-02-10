b160c5c417bdf05c6f69736c4f6aaefd40658fa5
Barış SAVAŞ <barissavas17@gmail.com>
2022-02-10 20:05:53 +0300

    solved

---
+++ 6kyu/equalSides.js
@@ -0,0 +1,19 @@
+function findEvenIndex(arr) {
+  var len = arr.length
+  if (len == 2 && arr[0] > arr[1]) return 0
+  for (let i = 0; i <= arr.length; i++) {
+    var result = 0;
+    let left = arr.slice(0, i).reduce((acc, sum) => acc + sum, 0)
+    let right = arr.slice(i - 1, len).reduce((acc, sum) => acc + sum, 0)
+    if (left == right) {
+      result += i;
+      break
+    }
+
+  }
+  if (result != -1) {
+    return result - 1
+  } else {
+    return result + 1
+  }
+}
