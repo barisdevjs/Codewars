da5b84a723b0a2eb17b452c49603b48cf84bc509
Barış SAVAŞ <barissavas17@gmail.com>
2022-02-25 00:34:11 +0300

    solved

--- 2kyu/insane coloured triangles.js
+++ 2kyu/insane coloured triangles.js
@@ -1,5 +1,4 @@
-
-function triangle(row) {                           // Needs a performance optimization //
+/* function triangle(row) {                           // Needs a performance optimization //
     let nextRow = [];
     let result = row.split("")
     for (var j=0; j < row.length-1; j++) {
@@ -16,14 +15,50 @@ function sumLetters(a, b) {
     switch (a+b) {
       case 'BG':
       case 'GB':
-      case 'RR': return 'R'; break;
+      case 'RR': return 'R'; 
       case 'BR':
       case 'RB':
-      case 'GG': return 'G'; break;
+      case 'GG': return 'G'; 
       case 'RG':
       case 'GR':
-      case 'BB': return 'B'; break;
+      case 'BB': return 'B'; 
     }
+} */
+
+
+const colors = ["R", "G", "B"];
+
+function combineColors(color1, color2) {
+  if (color1 === color2) {
+    return color1;
+  }
+
+  return colors.find(color => color !== color1 && color !== color2);
 }
 
+function log3(x) {
+  return Math.log(x) / Math.log(3);
+}
+
+function triangle(row) {
+  const { length } = row;
+
+  if (length === 1) {
+    return row[0];
+  }
+
+  const n = Math.floor(log3(length - 1));
+  const idealLength = Math.pow(3, n) + 1;
+
+  if (length === idealLength) {
+    return combineColors(row[0], row[length - 1]);
+  }
+
+  const size = length - idealLength + 1;
+  const firstColor = triangle(row.substring(0, size));
+  const secondColor = triangle(row.substring(length - size));
+
+  return combineColors(firstColor, secondColor);
+}
 console.log(triangle('RBRGBRB'))
+console.log(triangle('RBRGBRBGGRRRBGBBBGG'))
