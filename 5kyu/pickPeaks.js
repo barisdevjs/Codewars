d7eed12c06d69a790022dfe5cc64c8189802f40b
Barış SAVAŞ <barissavas17@gmail.com>
2022-11-30 21:12:18 +0300

    solved

--- 5kyu/pickPeaks.js
+++ 5kyu/pickPeaks.js
@@ -1,34 +1,59 @@
-function pickPeaks(arr) {
-    let output = {pos: [], peaks: []};
-    
-    if(arr.length === 0)
-      return output;
-    
-    let topPeak = arr[0];
-    let topPos = 0;
-    
-    for(let i=1; i<arr.length; i++){
-      
-      if(arr[i] > arr[i-1]) { // keep climbing
-        topPeak = arr[i];
-        topPos = i;
-        
-      } else if(arr[i] == arr[i-1]) { 
-          continue;
-      } else if(arr[i] < arr[i-1]) {
-        if(topPos > 0) {
-          output.pos.push(topPos);
-          output.peaks.push(topPeak);
-          topPos = 0;
-        } 
-      } else {
-        continue;
-      }
+function encode (input) {
+  return input.map(card => {
+    let [value, suit] = card.split('');
+    switch(value) {
+        case 'A': value =  0; break;
+        case 'T': value =  9; break;
+        case 'J': value = 10; break;
+        case 'Q': value = 11; break;
+        case 'K': value = 12; break;
+        default:  value = Number(value) - 1;
     }
     
-    return output;
-  }
-// console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]));
+    switch(suit) {
+        case 'c': suit = 0; break;
+        case 'd': suit = 13; break;
+        case 'h': suit = 26; break;
+        case 's': suit = 39; break;
+    }
+    
+    return value + suit;
+    
+  }).sort((a,b) => a - b);
+}
+
+function decode (input) {
+  return input.sort((a,b) => a - b).map(code => {
+    let value;
+    let suit;
+    
+    if (code < 13) {
+      suit = 'c';
+    }
+    else if (code < 26) {
+      code -= 13;
+      suit = 'd';
+    }
+    else if (code < 39) {
+      code -= 26;
+      suit = 'h';
+    }
+    else {
+      code -= 39;
+      suit = 's';
+    }
+    
+    switch(code) {
+      case 0: value = 'A'; break;
+      case 9: value = 'T'; break;
+      case 10: value = 'J'; break;
+      case 11: value = 'Q'; break;
+      case 12: value = 'K'; break;
+      default: value = (code + 1).toString();
+    }
+    
+    return value + suit;
+  });
+}
+console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]));
 console.log(pickPeaks([1,2,3,6,4,1,2,3,2,1]));
-// console.log(pickPeaks([1,2,5 ,4,3,2,3,6 ,4,1,2,3,3,4,5 ,3,2,1,2,3,5,5,4,3]));
-// {pos:[2,7,14,20], peaks:[5,6,5,5]}
