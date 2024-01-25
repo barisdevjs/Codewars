a89273ea164677d4bf4845429b31af51799d217f
Barış SAVAŞ <barissavas17@gmail.com>
2024-01-25 19:36:29 +0300

    solved

---
+++ 5kyu/largestDifferenceInIncreasingIndexes.js
@@ -0,0 +1,16 @@
+var largestDifference = function (data) {
+// find the max index difference in the data array
+    let maxDiff = 0;
+    for (let i = 0; i < data.length; i++) {
+        for (let j = i + 1; j < data.length; j++) {
+            if (data[j] - data[i] >= 0 && j-i >= maxDiff) {
+                maxDiff = j-i
+                j
+                i
+            }
+        }
+    }
+    return maxDiff;
+};
+
+console.log(largestDifference([9, 4, 1, 10, 3, 4, 0, -1, -2]))
