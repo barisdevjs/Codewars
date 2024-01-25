dc33669a8e3c0d078ebab33c705997d3eac9bd2b
Barış SAVAŞ <barissavas17@gmail.com>
2024-01-25 19:39:18 +0300

    kata refactored

--- 5kyu/largestDifferenceInIncreasingIndexes.js
+++ 5kyu/largestDifferenceInIncreasingIndexes.js
@@ -1,16 +1,16 @@
 var largestDifference = function (data) {
-// find the max index difference in the data array
-    let maxDiff = 0;
-    for (let i = 0; i < data.length; i++) {
-        for (let j = i + 1; j < data.length; j++) {
-            if (data[j] - data[i] >= 0 && j-i >= maxDiff) {
-                maxDiff = j-i
-                j
-                i
-            }
-        }
+  // find the max index difference in the data array
+  let maxDiff = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[j] >= data[i] && j - i >= maxDiff) {
+        maxDiff = j - i;
+        j;
+        i;
+      }
     }
-    return maxDiff;
+  }
+  return maxDiff;
 };
 
-console.log(largestDifference([9, 4, 1, 10, 3, 4, 0, -1, -2]))
+console.log(largestDifference([9, 4, 1, 10, 3, 4, 0, -1, -2]));
